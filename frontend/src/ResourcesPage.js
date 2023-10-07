fabd160aa2c9d6a1d49f4765913f0fd281c4b00d
fhuan10 <99567644+fhuan10@users.noreply.github.com>
2023-10-06 21:48:46 -0500

    Updated layout colors, video links

--- frontend/src/ResourcesPage.js
+++ frontend/src/ResourcesPage.js
@@ -1,90 +1,47 @@
-import './ResourcesPage.css';
+import "./ResourcesPage.css";
+import YouTube from 'react-youtube';
 
 function ResourcesPage() {
-
-    const handleClick = () => {
-        
-
-    }
-
+    const opts = {
+        height: '260',
+        width: '450',
+        playerVars: {
+          autoplay: 0, // Set to 1 if you want the video to autoplay
+        },
+      };
 
   return (
     <div className="resource-page">
-      <h2 className="resource-page-title">Resources</h2>
-
-      <div className="resource-page-filter">
-        <button onClick={handleClick}>Plumbing</button>
-        <button onClick={handleClick}>Painting</button>
-        <button onClick={handleClick}>Climbing</button>
-      </div>
-
-      <hr></hr>
-
-      <div className="resource-section">
-        <h3 className="resource-section-title">How to Paint the Wall</h3>
-        <div className="video-list">
-          <iframe
-            src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-            title="video"
-            className="video-item"
-          />
-
-          <iframe
-            src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-            title="video"
-            className="video-item"
-          />
-
-          <iframe
-            src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-            title="video"
-            className="video-item"
-          />
-        </div>
+      <div className="resource-page-header">
+        <h2 className="resource-page-title">Resources</h2>
+        <p>
+          Check out all of these videoes to either learn a new skill set or get
+          you prepared for volunteer days.
+        </p>
       </div>
       <hr></hr>
 
-      <div className="resource-section">
-        <h3 className="resource-section-title">How to Paint the Wall</h3>
-        <div className="video-list">
-          <iframe
-            src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-            title="video"
-            className="video-item"
-          />
+      <div className="resource-page-content">
+        <div className="resource-section">
+          <h3 className="resource-section-title">How to Paint</h3>
+          <div className="video-list">
+            <YouTube className="video-item" videoId="UdeZkJEv5xU" opts={opts} />
+            <YouTube className="video-item" videoId="syMnmDrp2qY" opts={opts} />
+            <YouTube className="video-item" videoId="39Cw42fWTmc" opts={opts} />
+          </div>
+        </div>
 
-          <iframe
-            src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-            title="video"
-            className="video-item"
-          />
+        <hr></hr>
 
-          <iframe
-            src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-            title="video"
-            className="video-item"
-          />
+        <div className="resource-section">
+          <h3 className="resource-section-title">How to Plumber</h3>
+          <div className="video-list">
+            <YouTube className="video-item" videoId="hlJUezuNFuI" opts={opts} />
+            <YouTube className="video-item" videoId="eVRVCtXCYu0" opts={opts} />
+            <YouTube className="video-item" videoId="Jt0VfS6xw2U" opts={opts} />
+          </div>
         </div>
       </div>
-
     </div>
   );
 }
