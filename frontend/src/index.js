3c7ee7b6048270099dc54f6860f4abe1b25fa605
kzhen <kzhen@cs.washington.edu>
2023-10-06 21:00:04 -0500

    cleaning up file structure + Rohan's login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Events from './Events';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Events />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
@@ -40,4 +20,3 @@ export default function App() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
->>>>>>> 4a2afa17ea186281b2494a74c510c4e058decbe6
