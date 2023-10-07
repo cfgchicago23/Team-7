4a2afa17ea186281b2494a74c510c4e058decbe6
kzhen <kzhen@cs.washington.edu>
2023-10-06 19:27:25 -0500

    Page routing

---
+++ frontend/src/Share.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// share RTA to social media page
+function Share() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Share Rebuilding Together Aurora</h1>
+        <button className="login-button">Login</button>
+      </header>
+      {}
+    </div>
+  );
+}
+
+export default Share;
