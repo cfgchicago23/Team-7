0ff69dce8be1ba4a38293af31a3106f286754be7
divyanitin3 <divyanitin@megamind.local>
2023-10-06 23:25:26 -0500

    added more to profile page

--- frontend/src/pages/profile_page.js
+++ frontend/src/pages/profile_page.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './profile_page.css'; // Import your CSS file
+import event_img from "./upcoming_events.jpg"
+import star_img from "./star.png"
 
 function Profile() {
     const handleShare = () => {
@@ -7,22 +9,105 @@ function Profile() {
     };
 
     return (
-        <div className="profile-container">
-            <div className="profile-image"></div>
-            <div className="profile-info">
-                <h2>John Doe</h2>
-                <p>Hours volunteered: <span className="star">⭐</span> 500</p>
-                <button className="share-button" onClick={handleShare}>
-                    Share
-                </button>
+        <div>
+            <div className="profile-container">
+                <div className="profile-image"></div>
+                <div className="profile-info">
+                    <h2>John Doe</h2>
+                    <div className="profile-text">
+                        <p>Hours volunteered: <span className="star">⭐</span> 500</p>
+                    </div>
+                    <button className="share-button" onClick={handleShare}>
+                        Share
+                    </button>
+                </div>
+                <div className="skills">
+                    <button className="skill-button">Painting</button>
+                    <button className="skill-button">Landscaping</button>
+                    <button className="skill-button">Carpentry</button>
+                    <button className="skill-button">Flooring</button>
+                    <button className="skill-button">General Volunteer</button>
+                </div>
             </div>
-            <div className="skills">
-                <button className="skill-button">Painting</button>
-                <button className="skill-button">Woodwork</button>
-                <button className="skill-button">Cleaning</button>
+            {/* Separator bar */}
+            <hr className="separator" />
+            {/* Additional green rectangle */}
+            <div className="additional-rectangle">
+                <h2>My Registered Events</h2>
+                <p>   </p>
+                <div className="contacts">
+        
+            <div className="contact-card">
+                <img src={event_img}/>
+                <h3>Mr. Whiskerson</h3>
+                <div className="info-group">
+                    <img src={star_img}/>
+                    <p>(212) 555-1234</p>
+                </div>
+                <div className="info-group">
+                    <img src={star_img} />
+                    <p>mr.whiskaz@catnap.meow</p>
+                </div>
+                <div className="tags">
+                            <button className="skill-button">Carpentry</button>
+                        </div>
             </div>
+            
+            <div className="contact-card">
+                <img src={event_img}/>
+                <h3>Fluffykins</h3>
+                <div className="info-group">
+                    <img src={star_img} />
+                    <p>(212) 555-2345</p>
+                </div>
+                <div className="info-group">
+                    <img src={star_img}/>
+                    <p>fluff@me.com</p>
+                </div>
+                <div className="tags">
+                            <button className="skill-button">Flooring</button>
+                        </div>
+            </div>
+            
+            <div className="contact-card">
+                <img src={event_img}/>
+                <h3>Felix</h3>
+                <div className="info-group">
+                    <img src={star_img} />
+                    <p>(212) 555-4567</p>
+                </div>
+                <div className="info-group">
+                    <img src={star_img} />
+                    <p>thecat@hotmail.com</p>
+                </div>
+                <div className="tags">
+                            <button className="skill-button">General Volunteer</button>
+                        </div>
+            </div>
+            
+            <div className="contact-card">
+                <img src={event_img}/>
+                <h3>Felix</h3>
+                <div className="info-group">
+                    <img src={star_img} />
+                    <p>(212) 555-4567</p>
+                </div>
+                <div className="info-group">
+                    <img src={star_img} />
+                    <p>thecat@hotmail.com</p>
+                </div>
+		                <div className="tags">
+                            <button className="skill-button">Painting</button>
+                        </div>
+
+            </div>
+            
+        </div>
+            </div>
+            
         </div>
     );
 }
 
 export default Profile;
+
