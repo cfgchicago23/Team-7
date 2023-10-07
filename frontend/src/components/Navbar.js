e7f64b89d692c1ce801b939c9e629c298a78cfed
Nandini Jirobe <njiro2@uic.edu>
2023-10-07 01:08:43 -0500

    adding navbar files

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import './Navbar.css'
 import logo from './rebuilding_aurora_logo.png';
-
+// navbar js
 export default function Navbar(){
     return <nav className = "nav">
         <img src={logo} alt="Logo" className="logo" />
