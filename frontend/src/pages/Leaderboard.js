07a164a6eda1702f6d82f8880cc28a5372de26e0
Rohan Gavankar <Rohan.Gavankar2004@gmail.com>
2023-10-07 09:24:57 -0500

    Added leaderboard

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -1,28 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Navbar from '../navbar';
-import './Leaderboard.css'
+
 function Leaderboard() {
-    const [players, setPlayers] = useState([]);
-    //call backend to get leaderboard
-    useEffect(() => {
-        axios.get('http://localhost:3001/leaderboard')
-            .then(response => setPlayers(response.data))
-            .catch(error => console.error("Error fetching data: ", error));
-    }, []);
     
+    const sampleData = [
+        { name: 'Alice', score: 20 },
+        { name: 'Bob', score: 18 },
+        { name: 'Charlie', score: 15 },
+        {name: 'Max', score: 5}
+        // ... add more players here
+    ];
+
     return (
-        //display leaderboard
-        
         <div>
             <Navbar />
             <header className="leaderboard-header">
                 <h1>Leaderboard</h1>
                 <ul className="leaderboard-list">
-                    {players.map(player => (
+                    {sampleData.map(player => (
                         <li key={player.id} className="player">
                             <span className="player-name">{player.name}</span>
-                            <span className="player-points">{player.points} pts</span>
+                            <span className="player-points">{player.score} pts</span>
                         </li>
                     ))}
                 </ul>
