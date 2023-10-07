d54c6e72bcd6bf6e4c48b7453f3cc832f7f2d9a6
fhuan10 <99567644+fhuan10@users.noreply.github.com>
2023-10-07 00:36:19 -0500

    Dymanically switch video by clicking video buttons

--- frontend/src/pages/resource_page.js
+++ frontend/src/pages/resource_page.js
@@ -3,6 +3,53 @@ import YouTube from "react-youtube";
 import { useState, useEffect } from "react";
 
 function Resources() {
+  const videoLinks = [
+    {"module-title": "Painting",
+    "videos": [{
+      "title": "a",
+      "video_id": "UdeZkJEv5xU"
+      },
+      {
+        "title": "b",
+        "video_id": "syMnmDrp2qY"
+      },
+      {
+        "title": "c",
+        "video_id": "39Cw42fWTmc"
+      }
+    ]},
+    {
+      "module-title": "Woodcutting",
+      "videos": [{
+        "title": "",
+        "video_id": "UdeZkJEv5xU"
+        },
+        {
+          "title": "e",
+          "video_id": "syMnmDrp2qY"
+        },
+        {
+          "title": "f",
+          "video_id": "39Cw42fWTmc"
+        }
+      ]},
+      {
+        "module-title": "Plumbering",
+        "videos": [{
+          "title": "g",
+          "video_id": "UdeZkJEv5xU"
+          },
+          {
+            "title": "h",
+            "video_id": "syMnmDrp2qY"
+          },
+          {
+            "title": "e",
+            "video_id": "39Cw42fWTmc"
+          }
+        ]}
+  ]
+
   const opts = {
     height: "260",
     width: "450",
@@ -13,6 +60,8 @@ function Resources() {
 
   const [selectedModule, setSelectedModule] = useState("Painting");
   const [moduleButtonColors, setModuleButtonColors] = useState(["#668A3D", "#FFFFFF", "#FFFFFF"]);
+  const [videoList, setVideoList] = useState(videoLinks[0].videos);
+  const [videoID, setVideoID] = useState("hlJUezuNFuI");
 
   const handleSelectedModule = (e) => {
     // store the value
@@ -20,15 +69,23 @@ function Resources() {
     console.log(selectedModule);
   }
 
+  const changeVideo =(youtubeID) => {
+    setVideoID(youtubeID);
+  }
+
   useEffect(() => {
+    // restart the colors
     let updatedModuleButtonColors = ["#FFFFFF", "#FFFFFF", "#FFFFFF"];
 
     if (selectedModule === "Painting") {
       updatedModuleButtonColors[0] = "#668A3D";
+      setVideoList(videoLinks[0].videos);
     } else if (selectedModule === "Woodcutting") {
       updatedModuleButtonColors[1] = "#668A3D";
+      setVideoList(videoLinks[1].videos);
     } else if (selectedModule === "Plumbering") {
       updatedModuleButtonColors[2] = "#668A3D";
+      setVideoList(videoLinks[2].videos);
     }
 
     setModuleButtonColors(updatedModuleButtonColors);
@@ -62,12 +119,16 @@ function Resources() {
         {/* Need to make this flex */}
         <div className="resource-module-container-contents">
           <div className="resource-module-video-list">
-
-
+            {videoList.map((video, index) => {
+              console.log(videoLinks)
+              return(
+                <button className="video-button" key={index} onClick={() => changeVideo(video.video_id)}>{video.title}</button>
+              )
+            })}
 
           </div>
 
-          <YouTube className="video-item" videoId="hlJUezuNFuI" opts={opts} />
+          <YouTube className="video-item" videoId={videoID} opts={opts} />
         </div>
       </div>
     </div>
