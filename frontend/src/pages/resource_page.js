78c4fa1a8204bffe2c6597b19dba83be161d2307
fhuan10 <99567644+fhuan10@users.noreply.github.com>
2023-10-07 00:03:32 -0500

    Recreated the resource page layout; added header and module-filter; add onClick to module-filter

--- frontend/src/pages/resource_page.js
+++ frontend/src/pages/resource_page.js
@@ -1,16 +1,42 @@
 import "./resource_page.css";
-import YouTube from 'react-youtube';
+import YouTube from "react-youtube";
+import { useState, useEffect } from "react";
 
 function Resources() {
-    const opts = {
-        height: '260',
-        width: '450',
-        playerVars: {
-          autoplay: 0, // Set to 1 if you want the video to autoplay
-        },
-      };
+  const opts = {
+    height: "260",
+    width: "450",
+    playerVars: {
+      autoplay: 0, // Set to 1 if you want the video to autoplay
+    },
+  };
+
+  const [selectedModule, setSelectedModule] = useState("Painting");
+  const [moduleButtonColors, setModuleButtonColors] = useState(["#668A3D", "#FFFFFF", "#FFFFFF"]);
+
+  const handleSelectedModule = (e) => {
+    // store the value
+    setSelectedModule(e.target.value);
+    console.log(selectedModule);
+  }
+
+  useEffect(() => {
+    let updatedModuleButtonColors = ["#FFFFFF", "#FFFFFF", "#FFFFFF"];
+
+    if (selectedModule === "Painting") {
+      updatedModuleButtonColors[0] = "#668A3D";
+    } else if (selectedModule === "Woodcutting") {
+      updatedModuleButtonColors[1] = "#668A3D";
+    } else if (selectedModule === "Plumbering") {
+      updatedModuleButtonColors[2] = "#668A3D";
+    }
+
+    setModuleButtonColors(updatedModuleButtonColors);
+
+  }, [selectedModule])
 
   return (
+
     <div className="resource-page">
       <div className="resource-page-header">
         <h2 className="resource-page-title">Resources</h2>
@@ -19,27 +45,29 @@ function Resources() {
           you prepared for volunteer days.
         </p>
       </div>
-      <hr></hr>
 
-      <div className="resource-page-content">
-        <div className="resource-section">
-          <h3 className="resource-section-title">How to Paint</h3>
-          <div className="video-list">
-            <YouTube className="video-item" videoId="UdeZkJEv5xU" opts={opts} />
-            <YouTube className="video-item" videoId="syMnmDrp2qY" opts={opts} />
-            <YouTube className="video-item" videoId="39Cw42fWTmc" opts={opts} />
-          </div>
+      <div className="resource-select-module-container">
+        <h3>Choose a module you want to learn!</h3>
+        
+        <div className="resource-select-module-buttons">
+          <form>
+            <input className="module-button" type="button" value="Painting" onClick={handleSelectedModule} style={{backgroundColor : moduleButtonColors[0]}}></input>
+            <input className="module-button" type="button" value="Woodcutting" onClick={handleSelectedModule} style={{backgroundColor : moduleButtonColors[1]}}></input>
+            <input className="module-button" type="button" value="Plumbering" onClick={handleSelectedModule} style={{backgroundColor : moduleButtonColors[2]}}></input>
+          </form>
         </div>
+      </div>
 
-        <hr></hr>
+      <div className="resource-module-container">
+        {/* Need to make this flex */}
+        <div className="resource-module-container-contents">
+          <div className="resource-module-video-list">
 
-        <div className="resource-section">
-          <h3 className="resource-section-title">How to Plumber</h3>
-          <div className="video-list">
-            <YouTube className="video-item" videoId="hlJUezuNFuI" opts={opts} />
-            <YouTube className="video-item" videoId="eVRVCtXCYu0" opts={opts} />
-            <YouTube className="video-item" videoId="Jt0VfS6xw2U" opts={opts} />
+
+
           </div>
+
+          <YouTube className="video-item" videoId="hlJUezuNFuI" opts={opts} />
         </div>
       </div>
     </div>
