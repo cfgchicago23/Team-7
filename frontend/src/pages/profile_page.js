226ab94d7892019747b45fcea846720b8291e0cc
divyanitin3 <divyanitin@megamind.local>
2023-10-07 00:47:54 -0500

    added events filter

--- frontend/src/pages/profile_page.js
+++ frontend/src/pages/profile_page.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './profile_page.css'; // Import your CSS file
 import event_img from "./upcoming_events.jpg"
 import star_img from "./star.png"
 
 function Profile() {
+    const [selectedTag, setSelectedTag] = useState(null);
+
     const handleShare = () => {
         alert('Share button clicked.'); // Replace this with your sharing logic
     };
 
+    // Sample event data
+    const events = [
+        {
+            name: "Veteran's Fall Clean-Up",
+            phone: "(212) 555-1234",
+            email: "a@gmail.com",
+            tags: ["General Volunteer", "Carpentry"]
+        },
+        {
+            name: "Clean-Up Event",
+            phone: "(212) 555-2345",
+            email: "becca@gmail.com",
+            tags: ["General Volunteer", "Flooring"]
+        },
+        {
+            name: "House Remodel Event",
+            phone: "(212) 555-4567",
+            email: "thecat@hotmail.com",
+            tags: ["General Volunteer", "Flooring"]
+        },
+        {
+            name: "Corporate Group Volunteering",
+            phone: "(212) 555-4567",
+            email: "thebusiness@hotmail.com",
+            tags: ["Painting", "Carpentry", "General Volunteer"]
+        },
+    ];
+
+    const filteredEvents = selectedTag
+        ? events.filter(event => event.tags.includes(selectedTag))
+        : events;
+
+    const skillButtons = ["Painting", "Landscaping", "Carpentry", "Flooring", "General Volunteer"];
+
     return (
         <div>
             <div className="profile-container">
-                <div className="profile-image"></div>
+            <div className="profile-image"></div>
                 <div className="profile-info">
-                    <h2>John Doe</h2>
+                    <h2>John Smith</h2>
                     <div className="profile-text">
                         <p>Hours volunteered: <span className="star">⭐</span> 500</p>
                     </div>
@@ -22,92 +58,47 @@ function Profile() {
                     </button>
                 </div>
                 <div className="skills">
-                    <button className="skill-button">Painting</button>
-                    <button className="skill-button">Landscaping</button>
-                    <button className="skill-button">Carpentry</button>
-                    <button className="skill-button">Flooring</button>
-                    <button className="skill-button">General Volunteer</button>
+                    {skillButtons.map(skill => (
+                        <button
+                            key={skill}
+                            className="skill-button"
+                            onClick={() => setSelectedTag(skill)}
+                        >
+                            {skill}
+                        </button>
+                    ))}
                 </div>
             </div>
-            {/* Separator bar */}
             <hr className="separator" />
-            {/* Additional green rectangle */}
             <div className="additional-rectangle">
                 <h2>My Registered Events</h2>
-                <p>   </p>
+                <p> </p>
                 <div className="contacts">
-        
-            <div className="contact-card">
-                <img src={event_img}/>
-                <h3>Veteran's Fall Clean-Up</h3>
-                <div className="info-group">
-                    <img src={star_img}/>
-                    <p>(212) 555-1234</p>
-                </div>
-                <div className="info-group">
-                    <img src={star_img} />
-                    <p>a@gmail.com</p>
-                </div>
-                <div className="tags">
-                            <button className="skill-button">Carpentry</button>
+                    {filteredEvents.map((event, index) => (
+                        <div className="contact-card" key={index}>
+                            <img src={event_img} alt="Event" />
+                            <h3>{event.name}</h3>
+                            <div className="info-group">
+                                <img src={star_img} alt="Star" />
+                                <p>{event.phone}</p>
+                            </div>
+                            <div className="info-group">
+                                <img src={star_img} alt="Star" />
+                                <p>{event.email}</p>
+                            </div>
+                            <div className="tags">
+                                {event.tags.map((tag, tagIndex) => (
+                                    <button className="skill-button" key={tagIndex}>
+                                        {tag}
+                                    </button>
+                                ))}
+                            </div>
                         </div>
+                    ))}
+                </div>
             </div>
-            
-            <div className="contact-card">
-                <img src={event_img}/>
-                <h3>Clean-Up Event</h3>
-                <div className="info-group">
-                    <img src={star_img} />
-                    <p>(212) 555-2345</p>
-                </div>
-                <div className="info-group">
-                    <img src={star_img}/>
-                    <p>becca@gmail.com</p>
-                </div>
-                <div className="tags">
-                            <button className="skill-button">Flooring</button>
-                        </div>
-            </div>
-            
-            <div className="contact-card">
-                <img src={event_img}/>
-                <h3>House Remodel Event</h3>
-                <div className="info-group">
-                    <img src={star_img} />
-                    <p>(212) 555-4567</p>
-                </div>
-                <div className="info-group">
-                    <img src={star_img} />
-                    <p>thecat@hotmail.com</p>
-                </div>
-                <div className="tags">
-                            <button className="skill-button">General Volunteer</button>
-                        </div>
-            </div>
-            
-            <div className="contact-card">
-                <img src={event_img}/>
-                <h3>Corporate Group Volunteering</h3>
-                <div className="info-group">
-                    <img src={star_img} />
-                    <p>(212) 555-4567</p>
-                </div>
-                <div className="info-group">
-                    <img src={star_img} />
-                    <p>thebusiness@hotmail.com</p>
-                </div>
-		                <div className="tags">
-                            <button className="skill-button">Painting</button>
-                        </div>
-
-            </div>
-            
-        </div>
-            </div>
-            
         </div>
     );
 }
 
 export default Profile;
-
