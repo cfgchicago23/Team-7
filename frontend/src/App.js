4fc8ff2d814225ee1b0a9bd6801a9d9b75e18393
DevinaT <devinat@uw.edu>
2023-10-07 08:13:30 -0700

    added color to homepage background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,16 +20,20 @@ function App() {
         <div className="image-container">
             <img src={volunteering} alt="Man volunteering" />
         </div>
-        <p>Together we repair homes, revitalize communities, and rebuild lives. Rebuilding Together Aurora is a safe and healthy housing organization that assists homeowners with critical home repairs, safety modifications, and energy efficiency upgrades. Our volunteers use tools like hammers and paint brushes to help qualified homeowners in need, playing a pivotal role in improving living conditions and invigorating the community.</p>
-        <p>For three decades, we've offered free essential home repairs to low-income Aurora residents including senior citizens, military veterans, individuals with disabilities, and families living in unsafe conditions. Our mission is to bridge the gap between homeowners and volunteers, ensuring everyone in Aurora has a safe and healthy living environment.</p>
+        <div className="para-1">
+          <p>Together we repair homes, revitalize communities, and rebuild lives. Rebuilding Together Aurora is a safe and healthy housing organization that assists homeowners with critical home repairs, safety modifications, and energy efficiency upgrades. Our volunteers use tools like hammers and paint brushes to help qualified homeowners in need, playing a pivotal role in improving living conditions and invigorating the community.</p>
+          <p>For three decades, we've offered free essential home repairs to low-income Aurora residents including senior citizens, military veterans, individuals with disabilities, and families living in unsafe conditions. Our mission is to bridge the gap between homeowners and volunteers, ensuring everyone in Aurora has a safe and healthy living environment.</p>
+        </div>
 
-        <h2>UPCOMING EVENTS</h2>
-        <h3>Veterans' Fall Clean-Up and Repairs</h3>
-        <p><strong>Date:</strong> Saturday, November 11, 2023</p>
-        <p><strong>Time:</strong> 8:00 AM - 1:00 PM</p>
-        <p><strong>Location:</strong> Rebuilding Together Aurora</p>
-        <p>Join Rebuilding Together Aurora and Crossroads Church for a special collaboration. Together, we'll honor the sacrifices made by our veterans and military families in the Fox Valley. Last year, our record-breaking volunteer turnout was able to assist more veterans, military families, and senior citizens than ever before. We aim to surpass those achievements in 2023.</p>
-        <p>If you'd like to register your team, click <a href="#">HERE</a>. And if you know of a veteran, an active-duty military member, a Gold Star family, or a service member’s widow who needs help with fall yard clean-up or light repairs, please contact us at <a href="mailto:support@rtaurora.org">support@rtaurora.org</a>.</p>
+        <div className="section-2">
+          <h2>UPCOMING EVENTS</h2>
+          <h3>Veterans' Fall Clean-Up and Repairs</h3>
+          <p><strong>Date:</strong> Saturday, November 11, 2023</p>
+          <p><strong>Time:</strong> 8:00 AM - 1:00 PM</p>
+          <p><strong>Location:</strong> Rebuilding Together Aurora</p>
+          <p>Join Rebuilding Together Aurora and Crossroads Church for a special collaboration. Together, we'll honor the sacrifices made by our veterans and military families in the Fox Valley. Last year, our record-breaking volunteer turnout was able to assist more veterans, military families, and senior citizens than ever before. We aim to surpass those achievements in 2023.</p>
+          <p>If you'd like to register your team, click <a href="#">HERE</a>. And if you know of a veteran, an active-duty military member, a Gold Star family, or a service member’s widow who needs help with fall yard clean-up or light repairs, please contact us at <a href="mailto:support@rtaurora.org">support@rtaurora.org</a>.</p>
+        </div>
       </section>
     </div>
   );
